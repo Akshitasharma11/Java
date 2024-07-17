1fe27f712fecbb07adef090c1d733b851af3326d
Akshita Sharma <akshitasharmab14@gmail.com>
2024-07-17 23:19:45 +0530

    change

--- first.java
+++ first.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class first {
-    
+    public static void main (String[] args){
+        @SuppressWarnings("resource")
+        Scanner num= new Scanner(System.in);
+        System.out.println("Enter a number you want to increase");
+        int a= num.nextInt();
+        System.out.println("Enter a number you want to decrease");
+        int b= num.nextInt();
+        System.out.print("a=");
+        System.out.println(++a);
+        System.out.print("b=");
+        System.out.println(--b);
+    }
+
 }
