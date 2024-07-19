b6350cded2d6adea40239e608ec4e0265976c7e2
Akshita Sharma <akshitasharmab14@gmail.com>
2024-07-19 23:39:38 +0530

    update

---
+++ if1.java
@@ -0,0 +1,22 @@
+// public class power {
+//     public static void main(String[] args) {
+//         int x=7;
+//         int a= x*49/7+35/7;
+//         System.out.println(a);
+//     }
+// }
+import java.util.*;
+
+public class if1 {
+    public static void main(String[] args) {
+        int age;
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("age=");
+            age=sc.nextInt();
+        
+        if (age>=18) {
+            System.out.println("Eligible");   
+        }
+    }
+}
--- power.java
+++
@@ -1,7 +0,0 @@
-public class power {
-    public static void main(String[] args) {
-        int x=7;
-        int a= x*49/7+35/7;
-        System.out.println(a);
-    }
-}
