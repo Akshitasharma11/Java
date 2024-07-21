1e6ca24faf92c35e61b8af0e432ae1c3d0ccb60a
Akshita Sharma <akshitasharmab14@gmail.com>
2024-07-21 22:22:32 +0530

    update

--- if.java
+++ if.java
@@ -1,10 +1,3 @@
-// public class power {
-//     public static void main(String[] args) {
-//         int x=7;
-//         int a= x*49/7+35/7;
-//         System.out.println(a);
-//     }
-// }
 import java.util.*;
 
 public class if1 {
