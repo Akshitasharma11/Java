26b082560ae2c8a68ad5f3a4b62db3a7627d90c9
Akshita Sharma <akshitasharmab14@gmail.com>
2024-08-05 22:15:50 +0530

    update

--- max.java
+++ max.java
@@ -1,18 +1,31 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 public class max {
     public static void main(String[] args){
-        @SuppressWarnings("resource")
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter two numbers to find max");
-        int a= sc.nextInt();
-        int b=sc.nextInt();
-        if(a>b){
-        System.out.print("max number=");
-        System.out.println(a);
-    }
-    else{
-        System.out.print("max number=");
-        System.out.println(b);
-    }
+    //  for(int i=4;i<0;i--){
+    //     for(int j=0;j<i;j++){
+        System.out.println("*");
+    //  }
+    //  System.out.println("/n");
+    // }
+
+
+
+
+
+
+
+    //     @SuppressWarnings("resource")
+    //     Scanner sc= new Scanner(System.in);
+    //     System.out.println("Enter two numbers to find max");
+    //     int a= sc.nextInt();
+    //     int b=sc.nextInt();
+    //     if(a>b){
+    //     System.out.print("max number=");
+    //     System.out.println(a);
+    // }
+    // else{
+    //     System.out.print("max number=");
+    //     System.out.println(b);
+    // }
     }
 }
