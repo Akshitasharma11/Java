6dbfd82d20e33cd4c9b32fa17010a5c5aa82967c
Akshita Sharma <akshitasharmab14@gmail.com>
2024-08-13 23:09:04 +0530

    update

--- switchcase.java
+++ switchcase.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class switchcase {
     public static void main (String[] args){
         int n;
-         @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
          System.out.println("enter a number");
          n= sc.nextInt();
