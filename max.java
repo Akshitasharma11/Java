8dd181949ebd506fed2d67c4ccae1ab641a50c94
Akshita Sharma <akshitasharmab14@gmail.com>
2024-08-15 22:45:47 +0530

    update

--- max.java
+++ max.java
@@ -4,7 +4,7 @@ public class max {
         int n=4;
      for(int i=n;i>0;i--){
         for(int j=0;j<i;j++){
-        System.out.print("*");
+        System.out.print(" * ");
      }
         System.out.print("\n");
     }
